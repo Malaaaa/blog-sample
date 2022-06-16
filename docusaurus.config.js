4bbf0ea44cbfc18b3d26e0468cb2279754f44f9a
Malaaaa <37627837+Malaaaa@users.noreply.github.com>
2022-06-16 19:50:10 -0400

    feat: change logo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,7 +10,7 @@ const config = {
   baseUrl: "/",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
-  favicon: "img/favicon.ico",
+  favicon: "img/favicon.png",
   organizationName: "malaaaa", // Usually your GitHub org/user name.
   projectName: "malaaaa.github.io", // Usually your repo name.
 
